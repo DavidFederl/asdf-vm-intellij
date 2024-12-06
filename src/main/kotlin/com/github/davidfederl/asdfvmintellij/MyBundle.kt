41067ccab36c6f434c23e8303a420fb2c5e34165
GitHub Action <action@github.com>
2024-12-06 19:13:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/davidfederl/asdfvmintellij/MyBundle.kt
+++ src/main/kotlin/com/github/davidfederl/asdfvmintellij/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.davidfederl.asdfvmintellij
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/davidfederl/asdfvmintellij/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/davidfederl/asdfvmintellij/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.davidfederl.asdfvmintellij.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
